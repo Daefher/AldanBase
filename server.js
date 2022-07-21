8402a3c6d8baa725397088e7a830e48f2b8c1030
Daniel Flores <daefher@gmail.com>
2022-07-21 16:25:47 -0500

    cors policy added

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 //Install express server
 const express = require('express');
 const path = require('path');
+var cors = require('cors');
+
 
 const app = express();
 
@@ -11,6 +13,7 @@ app.get('/*', function(req,res) {
     
 res.sendFile(path.join(__dirname+'/dist/AldanBase/index.html'));
 });
+app.use(cors());
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
