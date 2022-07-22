5b61416074a30b0968e19557debe746520c19cc3
Daniel Flores <daefher@gmail.com>
2022-07-22 11:00:37 -0500

    change

--- server.js
+++ server.js
@@ -11,10 +11,10 @@ var corsOptions = {
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
   }
 
-app.use(cors(corsOptions));
+app.use(cors());
 
 // Serve only the static files form the dist directory
-//app.use(express.static(__dirname + '/dist/AldanBase'));
+app.use(express.static(__dirname + '/dist/AldanBase'));
 
 app.get('/*', function(req,res) {
     
