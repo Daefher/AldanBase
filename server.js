3845128baa1d5b5241f1b22097cdb51696ac64eb
Daniel Flores <daefher@gmail.com>
2022-07-22 11:11:11 -0500

    test

--- server.js
+++ server.js
@@ -11,7 +11,24 @@ var corsOptions = {
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
   }
 
-app.use(cors());
+//app.use(cors());
+//Cors Configuration - Start
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested, Content-Type, Accept Authorization"
+  )
+  if (req.method === "OPTIONS") {
+    res.header(
+      "Access-Control-Allow-Methods",
+      "POST, PUT, PATCH, GET, DELETE"
+    )
+    return res.status(200).json({})
+  }
+  next()
+})
+//Cors Configuration - End
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/AldanBase'));
