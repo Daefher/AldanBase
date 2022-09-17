79a50569e1f6b481900494ac2e3828bdf6ed34a6
Daniel Flores <daefher@gmail.com>
2022-09-16 22:50:01 -0500

    Orders details, and orders logic

---
+++ src/app/interfaces/product-qty.ts
@@ -0,0 +1,19 @@
+export interface ProductQty {
+
+    
+        aldanCustomerId: number,
+        companyId: number,
+        plantId: number,
+        partQtyId: number,
+        partId: number,
+        onHandQty: number,
+        createdBy: number,
+        createdDateTime: string,
+        canceledBy: 0,
+        canceledDateTime: string,
+        canceled: boolean,
+        cancelable: boolean,
+        sysRowId: string,
+        uomId: number
+      
+}
