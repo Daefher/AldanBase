42ae760a4c2cb58612995a0ebcda758eb579c5cf
Daniel Eduardo Flores Hernández <daefher@gmail.com>
2023-06-24 22:05:50 -0600

    AuthGuard for pages

--- src/app/modules/management/management-routing.module.ts
+++ src/app/modules/management/management-routing.module.ts
@@ -5,13 +5,14 @@ import { InventoryViewComponent } from './components/inventory-view/inventory-vi
 import { CompanyNameResolver } from 'src/app/resolvers/company-name/company-name.resolver';
 import { CompanyResolver } from 'src/app/resolvers/company-resolver/company.resolver';
 import { OrdersDetailViewComponent } from './components/orders-detail-view/orders-detail-view.component';
+import { AuthGuardGuard } from 'src/app/helpers/auth-guard.guard';
 
 const routes: Routes = [
   {
     path: '', component: ManagementComponent, resolve: { company: CompanyResolver }, title: CompanyNameResolver,
     children: [
-      { path: 'products', component: InventoryViewComponent, resolve: { company: CompanyResolver }, title: CompanyNameResolver },
-      { path: 'orders-detail', component: OrdersDetailViewComponent, resolve: { company: CompanyResolver }, title: CompanyNameResolver }
+      { path: 'products', component: InventoryViewComponent, canActivate: [AuthGuardGuard], resolve: { company: CompanyResolver }, title: CompanyNameResolver },
+      { path: 'orders-detail', component: OrdersDetailViewComponent, canActivate: [AuthGuardGuard], resolve: { company: CompanyResolver }, title: CompanyNameResolver }
     ]
   }
 ];
