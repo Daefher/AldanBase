b1dd108ddf0eb1795b6a7e405b6b742f5fae9144
Daniel Eduardo Flores Hernández <daefher@gmail.com>
2023-07-12 19:23:59 -0600

    Info view and compoenent changes

--- src/app/modules/company-info/components/company-info-view/company-info-view.component.ts
+++ src/app/modules/company-info/components/company-info-view/company-info-view.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ActivatedRoute, Data } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { CompanyInterface } from 'src/app/interfaces/company-interface';
+import { BannerService } from 'src/app/services/demo-banner/banner.service';
+import { AuthenticationService } from 'src/app/services/demo-login/authentication.service';
+import { UsersService } from 'src/app/services/demo-user/users.service';
 
 @Component({
   selector: 'app-company-info-view',
   templateUrl: './company-info-view.component.html',
   styleUrls: ['./company-info-view.component.scss']
 })
-export class CompanyInfoViewComponent {
+export class CompanyInfoViewComponent implements OnInit{
 
+
+  company: CompanyInterface;
+  constructor(
+    public authenticationService: AuthenticationService,
+    public usersService: UsersService,
+    private toastr: ToastrService,
+    public dialog: MatDialog,
+    private route: ActivatedRoute,
+    private fileService : BannerService
+    
+  ){
+  }
+
+  ngOnInit(): void {
+    this.route.data.subscribe((response: Data) => {
+      this.company = response.company[0];
+    });
+  }
 }
